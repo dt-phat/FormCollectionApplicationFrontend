ebb32fe6499d3be3a51070565f352ea4926c05a7
dt-phat <phatb2203522@student.ctu.edu.vn>
2025-02-23 09:39:37 +0700

    first commit

---
+++ src/api/axios.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://localhost:8080",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+api.interceptors.request.use(
+    (config) => {
+        console.log("Đang gửi request:", config);
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+    (response) => response.data,
+    (error) => Promise.reject(error)
+);
+
+export default api;
