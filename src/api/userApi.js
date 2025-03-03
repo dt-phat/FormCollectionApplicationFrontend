263f9bf58ff6fecc0303debce46b3056a8e15853
trietle3535 <trietb2203535@student.ctu.edu.vn>
2025-03-03 22:24:07 +0700

    update user

---
+++ src/api/userApi.js
@@ -0,0 +1,35 @@
+import api from "./axios"; 
+
+// lấy danh sách người dùng
+export const getUsers = async () => {
+  try {
+    const response = await api.get("/users"); 
+    return response; 
+  } catch (error) {
+    console.error("Lỗi khi lấy danh sách người dùng:", error);
+    throw error; 
+  }
+};
+
+// xóa người dùng
+export const deleteUser = async (ID) => {
+  try {
+    await api.delete(`/users/${ID}`);
+  }
+  catch (error) {
+    console.error("Lỗi khi xóa :", error);
+    throw error;
+  }
+};
+
+// sửa người dùng 
+export const updateUser = async (id, updateData) => {
+  try {
+    const response = await api.put(`/users/${id}`,updateData);
+    return response.data;
+  }
+  catch (error) {
+    console.log(` Lỗi khi cập nhật`,error);
+    throw error;
+  }
+};
