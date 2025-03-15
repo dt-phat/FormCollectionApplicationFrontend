ff74fa8fdd841d547d4c9eb298657e0e74330829
trietle3535 <trietb2203535@student.ctu.edu.vn>
2025-03-15 17:45:56 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,8 @@ export default {
             hasNavBarPage: [
                 "/",
                 "/project",
-                "/project/:projectId"
+                "/project/:projectId",
+                "/users"
             ],
             hasFooterPages: [
                 "/",
